ea8be42dc41d7d6435576d8e184470af04a25c74
NirbheyJaijn <nirbhey@hotmail.co.uk>
2020-02-22 20:32:46 +0000

    chat messagign layout

---
+++ JunctionXAmi/app/src/main/java/com/example/junctionx_ami/MessageAMI.kt
@@ -0,0 +1,14 @@
+package com.example.junctionx_ami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageAMI : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messageami)
+    }
+
+    
+}
