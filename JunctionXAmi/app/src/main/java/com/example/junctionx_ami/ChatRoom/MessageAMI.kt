38fd4e49ffbf0306934cb7f6aa133887e5889472
NirbheyJaijn <nirbhey@hotmail.co.uk>
2020-02-23 06:09:47 +0000

    placeholder images replaced, code cleaned

--- JunctionXAmi/app/src/main/java/com/example/junctionx_ami/ChatRoom/MessageAMI.kt
+++ JunctionXAmi/app/src/main/java/com/example/junctionx_ami/ChatRoom/MessageAMI.kt
@@ -1,44 +1,3 @@
-//package com.example.junctionx_ami
-//
-//import android.os.Bundle
-//import android.widget.EditText
-//import androidx.appcompat.app.AppCompatActivity
-//import com.scaledrone.lib.Room
-//import com.scaledrone.lib.RoomListener
-//import com.scaledrone.lib.Scaledrone
-//
-//class MessageAMI : AppCompatActivity(), RoomListener {
-//
-//    private val channelID = "aMMwbysHiap1Swx2"
-////    private val roomName = "observable-room"
-//    private var editText: EditText? = null
-//    private val scaledrone: Scaledrone? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_messageami)
-//        // This is where we write the message
-//        editText = findViewById(R.id.editText) as EditText
-//    }
-//
-//    // Successfully connected to Scaledrone room
-//    override fun onOpen(room: Room) {
-//        println("Scaledrone connection open")
-////        println("Connected to room")
-//    }
-//
-//    // Connecting to Scaledrone room failed
-//    override fun onOpenFailure(room: Room, ex: Exception) {
-//        System.err.println(ex)
-//    }
-//
-//    // Received a message from Scaledrone room
-//    override fun onMessage(room: Room, receivedMessage: com.scaledrone.lib.Message) {
-//        // TODO
-//    }
-//
-//}
-
 package com.example.scaledrone.chat
 
 //import android.R.*
@@ -141,22 +100,3 @@ class MessageAMI : AppCompatActivity(), RoomListener {
 
     }
 }
-
-//internal class MemberData {
-//    val name: String
-//    val color: String
-//
-//    constructor(name: String, color: String) {
-//        this.name = name
-//        this.color = color
-//    }
-//
-//    constructor() {}
-//
-//    override fun toString(): String {
-//        return "MemberData{" +
-//                "name='" + name + '\''.toString() +
-//                ", color='" + color + '\''.toString() +
-//                '}'.toString()
-//    }
-//}
--- JunctionXAmi/app/src/main/java/com/example/junctionx_ami/Welcome.kt
+++ JunctionXAmi/app/src/main/java/com/example/junctionx_ami/Welcome.kt
@@ -13,32 +13,10 @@ import com.igalata.bubblepicker.model.BubbleGradient
 import com.igalata.bubblepicker.model.PickerItem
 import com.igalata.bubblepicker.rendering.BubblePicker
 import kotlinx.android.synthetic.main.activity_welcome.*
+import java.lang.Math.random
 
 
 class Welcome : AppCompatActivity() {
-
-//    val emotionSet = arrayListOf<String>(
-//        "Happy", "Depressed", "Anxious", "Angry", "Content",
-//        "Excited", "Productive", "Bored", "Tired", "Sad", "Stressed"
-//    )
-//    val goodEmotes = arrayListOf<String>("Happy", "Excited", "Productive", "Content")
-//    val badEmotes = arrayListOf<String>("Depressed", "Anxious", "Angry", "Bored", "Sad", "Stressed")
-
-
-//    val emoteBubbes = arrayListOf<Drawable>(R.drawable.happy, R.drawable.depressed, R.drawable.anxious,
-//                                     R.drawable.angry, R.drawable.content, R.drawable.excited,
-//                                     R.drawable.productive, R.drawable.bored, R.drawable.tired,
-//                                     R.drawable.sad, R.drawable.stressed)
-//
-//
-//    val bubbleColors = arrayListOf<Int>(
-//        Color.parseColor("#E3DFFF"),
-//        Color.parseColor("A4DEF9"),
-//        Color.parseColor("#EFBCD5"),
-//        Color.parseColor("#AED9E0"),
-//        Color.parseColor("#C59FC9")
-//    )
-
 
     private val boldTypeface by lazy { Typeface.createFromAsset(assets, ROBOTO_BOLD) }
     private val mediumTypeface by lazy { Typeface.createFromAsset(assets, ROBOTO_MEDIUM) }
@@ -54,11 +32,8 @@ class Welcome : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        titleTextView.typeface = mediumTypeface
-        subtitleTextView.typeface = boldTypeface
         hintTextView.typeface = regularTypeface
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            subtitleTextView.letterSpacing = 0.06f
             hintTextView.letterSpacing = 0.05f
         }
 
@@ -73,8 +48,8 @@ class Welcome : AppCompatActivity() {
             override fun getItem(position: Int): PickerItem {
                 return PickerItem().apply {
                     title = emotions[position]
-                    gradient = BubbleGradient(colors.getColor((position * 2) % 8, 0),
-                        colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL)
+                    gradient = BubbleGradient(colors.getColor((random().toInt() * 2) % 8, 1),
+                        colors.getColor((random().toInt() * 2) % 8 + 2, 4), BubbleGradient.HORIZONTAL)
                     typeface = mediumTypeface
                     textColor = ContextCompat.getColor(this@Welcome, android.R.color.white)
                     backgroundImage = ContextCompat.getDrawable(this@Welcome, images.getResourceId(position, 0))
@@ -85,7 +60,7 @@ class Welcome : AppCompatActivity() {
         colors.recycle()
         images.recycle()
 
-        picker.bubbleSize = 20
+        picker.bubbleSize = 45
         picker.listener = object : BubblePickerListener {
             override fun onBubbleSelected(item: PickerItem) = toast("${item.title} selected")
 
@@ -107,33 +82,5 @@ class Welcome : AppCompatActivity() {
 
 }
 
-
-//
-//    var bubblePickerListener = object : BubblePickerListener {
-//        override fun onBubbleSelected(item: PickerItem) {
-//            Toast.makeText(applicationContext, "" + item.title + " selected", Toast.LENGTH_SHORT)
-//                .show()
-//
-//        }
-//
-//        override fun onBubbleDeselected(item: PickerItem) {
-//            Toast.makeText(applicationContext, "" + item.title + " DeSelected", Toast.LENGTH_SHORT)
-//                .show()
-//
-//        }
-//    }
-
-
-//    val bubblePicker : BubblePicker = findViewById(R.id.picker)
-//
-//    val num_emotions = emotionSet.size
-//    var listItems = ArrayList<PickerItem>()
-//    for (e in 0 until num_emotions){
-//        var item : PickerItem = PickerItem(title = emotionSet[e], color = bubbleColors[e], textColor = Color.WHITE)
-//        listItems.add(item)
-//    }
-//
-//    bubblePicker.selectedItems
-//}
 
 
